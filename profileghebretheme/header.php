86fbe7fd6211fca6634730c73e14b9bbe4328007
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-23 21:30:52 -0800

    same

--- profileghebretheme/header.php
+++ profileghebretheme/header.php
@@ -42,9 +42,9 @@
                <div class="search">
 					<form><?php get_sidebar('header'); ?></form>
                     <ul> 
-						<li><a href="https://www.facebook.com/ghebrehiwet.berhane"><img src="<?php bloginfo('template_url'); ?>/images/facebook11.png" alt="facebook icon"></a></li>
-						<li><a href="http://www.linkedin.com/pub/ghebrehiwet-ghebremedhin/40/631/89a?trk=pub-pbmap"><img src="<?php bloginfo('template_url'); ?>/images/linkedin7.png" alt="linkedIn"></a></li>
-						<li><a href="https://github.com/Ghebrehiwet"><img src="<?php bloginfo('template_url'); ?>/images/github7.png" alt="GitHub"></a></li> 
+						<li><a href="https://www.facebook.com/ghebrehiwet.berhane" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/facebook11.png" alt="facebook icon"></a></li>
+						<li><a href="http://www.linkedin.com/pub/ghebrehiwet-ghebremedhin/40/631/89a?trk=pub-pbmap"  target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/linkedin7.png" alt="linkedIn"></a></li>
+						<li><a href="https://github.com/Ghebrehiwet"  target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/github7.png" alt="GitHub"></a></li> 
 					</ul>
                      
                  </div>
