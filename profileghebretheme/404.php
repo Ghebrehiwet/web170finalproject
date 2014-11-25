9e8cbfd869967eef5861174badd65e584b499db4
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-25 01:31:26 -0800

    404.php

--- profileghebretheme/404.php
+++ profileghebretheme/404.php
@@ -4,24 +4,22 @@
 
 
 <div id="middle"><!--opening #middle -->
-				<aside id="sidebar-left">
-					 
-					<?php get_sidebar('secondary'); ?>
-				</aside>
+	<aside id="sidebar-left"> 
+		<?php get_sidebar('secondary'); ?>
+	</aside>
 <!-- END SIDEBAR -->
                 
 <!-- START CONTENT -->
 	<div id="main"><!--open #main --> 
-		<div id="content" class="404">
-
+		<div id="content" class="404"> 
 			<h2>Error 404</h2>
 			<p>Sorry, the page you are looking for doesn't exist.</p>
 		</div>
-				<div id="sidebar-right">
-						<div id="widgets">
-								<?php get_sidebar('primary'); ?>
-						</div>
-				</div>
+		<div id="sidebar-right">
+			<div id="widgets">
+				<?php get_sidebar('primary'); ?>
+			</div>
+		</div>
 <!-- END CONTENT -->
 
 	</div>
