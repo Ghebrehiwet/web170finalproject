5b3a7aa20521de5b676a20ce0160d116a3b8ac46
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-24 22:36:14 -0800

    search.php

--- profileghebretheme/search.php
+++ profileghebretheme/search.php
@@ -1,29 +1,43 @@
 <?php get_header(); ?>
 
 <!-- START SIDEBAR -->
-<?php get_sidebar('primary'); ?>
+
+
+<div id="middle"><!--opening #middle -->
+				<aside id="sidebar-left">
+					 
+					<?php get_sidebar('secondary'); ?>
+				</aside>
 <!-- END SIDEBAR -->
-
+                
 <!-- START CONTENT -->
+	<div id="main"><!--open #main --> 
 
-<div id="content" class="search">
+			<?php if (have_posts()) : ?>
+				 <div id="content" class="index">
+				<h2 class="pagetitle">Search Results</h2>
+				<ul>
+					<?php while (have_posts()) : the_post(); ?>
+					<li id="post-<?php the_ID(); ?>"><?php the_time('F jS, Y') ?> - <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></li>
+					<?php endwhile; ?>
+				</ul>
+			<?php else : ?>
 
-	<?php if (have_posts()) : ?>
+				<h2>No posts found. Try a different search?</h2>
+				<?php get_search_form(); ?>
+			</div>
+		<?php endif; ?>
+				
 
-		<h2 class="pagetitle">Search Results</h2>
-		<ul>
-		<?php while (have_posts()) : the_post(); ?>
-		<li id="post-<?php the_ID(); ?>"><?php the_time('F jS, Y') ?> - <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></li>
-		<?php endwhile; ?>
-		</ul>
-	<?php else : ?>
+					<div id="sidebar-right">
+						<div id="widgets">
+								<?php get_sidebar('primary'); ?>
+						</div>
+					</div>
+		<!-- END CONTENT -->
 
-		<h2>No posts found. Try a different search?</h2>
-    	<?php get_search_form(); ?>
+			
+										
+	</div>
 
-	<?php endif; ?>
-		
-</div>
-<!-- END CONTENT -->
-<?php get_sidebar('secondary'); ?>
 <?php get_footer(); ?>
