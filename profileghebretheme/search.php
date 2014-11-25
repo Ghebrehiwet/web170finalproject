a1384a37b4567299f652160b4a7c4431c734e481
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-24 22:47:50 -0800

    search.php

--- profileghebretheme/search.php
+++ profileghebretheme/search.php
@@ -14,25 +14,26 @@
 
 			<?php if (have_posts()) : ?>
 				 <div id="content" class="index">
-				<h2 class="pagetitle">Search Results</h2>
-				<ul>
-					<?php while (have_posts()) : the_post(); ?>
-					<li id="post-<?php the_ID(); ?>"><?php the_time('F jS, Y') ?> - <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></li>
-					<?php endwhile; ?>
-				</ul>
-			<?php else : ?>
+						<h2 class="pagetitle">Search Results</h2>
+						<ul>
+							<?php while (have_posts()) : the_post(); ?>
+							<li id="post-<?php the_ID(); ?>"><?php the_time('F jS, Y') ?> - <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></li>
+							<?php endwhile; ?>
+						</ul>
+					<?php else : ?>
 
-				<h2>No posts found. Try a different search?</h2>
-				<?php get_search_form(); ?>
+						<h2>No posts found. Try a different search?</h2>
+						<?php get_search_form(); ?>
+				</div>
+			<div id="sidebar-right">
+				<div id="widgets">
+					<?php get_sidebar('primary'); ?>
+				</div>
 			</div>
 		<?php endif; ?>
 				
 
-					<div id="sidebar-right">
-						<div id="widgets">
-								<?php get_sidebar('primary'); ?>
-						</div>
-					</div>
+					
 		<!-- END CONTENT -->
 
 			
