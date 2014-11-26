7a458ad487f08009cba382c2b82d07ec7cffb37a
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-26 01:28:28 -0800

    footer.php

--- profileghebretheme/footer.php
+++ profileghebretheme/footer.php
@@ -19,13 +19,14 @@
 				</div>
 				
 				<div id="copyright">
-					<?php date('Y'); ?>
+					 
 					<ul>  
                         <li><a href="http://http://www.gebgebonline.com/myprofile/wp-login.php">login</a></li>
                         <li><a href="http://www.gebgebonline.com/myprofile/contact-me/">Contact</a></li>
 						<li><a href="https://www.facebook.com/ghebrehiwet.berhane" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/facebook11.png" alt="facebook icon"></a></li>
 						<li><a href="http://www.linkedin.com/pub/ghebrehiwet-ghebremedhin/40/631/89a?trk=pub-pbmap"  target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/linkedin7.png" alt="linkedIn"></a></li>
 						<li><a href="https://github.com/Ghebrehiwet"  target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/github7.png" alt="GitHub"></a></li>
+						<li>Copy&copy<?php date('Y'); ?></li>
 					</ul>
 				
 				</div>
