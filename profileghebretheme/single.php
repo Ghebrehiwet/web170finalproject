5a79b67b07e3e44d2d15741ec46c781504555ecf
ghebre ghebremedhin <geberhane@gmail.com>
2014-11-28 18:49:11 -0800

    single.php

--- profileghebretheme/single.php
+++ profileghebretheme/single.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?> 
 
 <div id="middle"><!--opening #middle -->
-				<aside id="sidebar-left"> 
+				<aside id="sidebar-left">
+					 
 					<?php get_sidebar('secondary'); ?>
 				</aside>
  
@@ -10,7 +11,7 @@
                 <div id="content" class="index">
                     <?php while (have_posts()) : the_post(); ?>
                         <div class="post-box">
-                         
+                        <h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
                         <p class="postmetadata">
                             <span class="date">Posted on <?php the_time('F jS, Y') ?></span> in 
                             <span class="cat"><?php the_category(', ') ?></span>
