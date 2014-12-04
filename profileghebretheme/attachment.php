6c2beeac1ef00530772472e247f41dd64794c5b0
Ghebre Ghebremedhin <geberhane@gmail.com>
2014-12-03 21:23:01 -0800

    attachment.php

--- profileghebretheme/attachment.php
+++ profileghebretheme/attachment.php
@@ -1,13 +1,19 @@
 <?php get_header(); ?>
 
-<!-- START SIDEBAR -->
-<?php get_sidebar(); ?>
-<!-- END SIDEBAR -->
-
-<!-- START CONTENT -->
-<div id="content" class="attachment">
-<strong class="breadcrumb"><?php if (function_exists('bcn_display')) { bcn_display(); } ?></strong>
-<div id="gallery-box" class="entrytext"> 
+<div id="middle"><!--opening #middle -->
+		<aside id="sidebar-left"> 
+			<?php get_sidebar('secondary'); ?>
+		</aside>
+ 
+	<div id="main"><!--open #main --> 
+			<div class="breadcrumbs">
+					<?php if(function_exists('bcn_display'))
+					{
+						bcn_display();
+					}?>
+				</div>
+				
+<div id="content" class="attachment"> 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <?php $attachment_link = get_the_attachment_link($post->ID, true, array(523, 1000)); // This also populates the iconsize for the next line ?>
 <?php $_post = &get_post($post->ID); $classname = ($_post->iconsize[0] <= 50 ? 'small' : '') . 'attachment'; // This lets us style narrow icons specially ?>
@@ -22,13 +28,19 @@
 				<?php link_pages('<p><strong>Pages:</strong> ', '</p>', 'number'); ?>
 				<p class="postmetadata alt clear"><small><?php edit_post_link('Edit this entry.','',''); ?></small></p>
 		</div>
-</div>
+		 
+	</div>
         
 	<?php comments_template(); ?>
 	<?php endwhile; else: ?>
 		<p>Sorry, no attachments matched your criteria.</p>
 	<?php endif; ?>
 </div>
+			<div id="sidebar-right">
+						<div id="widgets">
+								<?php get_sidebar('primary'); ?>
+						</div>
+				</div>
 <!-- END CONTENT -->
-
+</div>
 <?php get_footer(); ?>
