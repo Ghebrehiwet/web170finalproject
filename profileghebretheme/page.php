fa56d1e768a9847a417d4a5917dcd1ae768e06c0
Ghebre Ghebremedhin <geberhane@gmail.com>
2014-12-08 18:03:02 -0800

    adding thumbnail tag

--- profileghebretheme/page.php
+++ profileghebretheme/page.php
@@ -18,7 +18,7 @@
                     <?php while (have_posts()) : the_post(); ?>
                         <div class="post-box">
                         <h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
-                         
+                         <?php the_post_thumbnail(); ?>
                         <?php the_content('More &raquo;'); ?>
                         <?php edit_post_link('Edit.', '<p><small>', '</small></p>'); ?>
                         </div>
