8eefb9bb258aed34d6fc72c7d6b9a96b24e4fa7a
Ghebre Ghebremedhin <geberhane@gmail.com>
2014-12-08 16:56:19 -0800

    removing validation images

--- profileghebretheme/footer.php
+++ profileghebretheme/footer.php
@@ -1,13 +1,12 @@
 				<footer>
 				<div id="validation">
-					<p>
-						<a href="http://validator.w3.org/check?uri=referer" target="_blank">
-						  <img src="http://www.w3.org/html/logo/badge/html5-badge-h-solo.png" width="31" height="32" alt="valid HTML5">
-						 </a>
-						 <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3" target="_blank">
-						  <img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS3" height="31" width="88">
-						</a> 
-					</p>
+						<h1>
+						  <a href="<?php echo get_option('home'); ?>">
+							<span><?php bloginfo('name'); ?> </span><!-- for SEO -->
+							<img src="<?php bloginfo('template_url'); ?>/images/myimg2.JPG" id="logo"  
+							  alt="My Logo"> <!-- for screen readers --> 	
+						  </a>
+					</h1> 
 				</div>
 				
 				<div id="copyright">
